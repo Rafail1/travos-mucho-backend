80651be18cf1b96d911a01bcaf020f7458de0381
Rafail1 <ya.rafon-92@ya.ru>
2023-08-14 11:39:37 +0300

    ping/pong

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DepthModule } from './modules/history/depth/depth.module';
 import { TradesModule } from './modules/history/trades/trades.module';
 import { WebSocketModule } from './modules/websocket/websocket.module';
 
 @Module({
-  imports: [DepthModule, WebSocketModule, TradesModule],
+  imports: [WebSocketModule, TradesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/history/trades/trades.service.ts
+++ src/modules/history/trades/trades.service.ts
@@ -1,5 +1,6 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { client as WebSocketClient } from 'websocket';
+import { firstValueFrom } from 'rxjs';
 import { DatabaseService } from '../../database/database.service';
 import {
   AggTrade,
@@ -7,28 +8,52 @@ import {
   WebSocketService,
 } from '../../websocket/websocket.service';
 
-const BUFFER_LENGTH = 1000;
+const AGG_TRADES_BUFFER_LENGTH = 1000;
+const DEPTH_BUFFER_LENGTH = 1000;
 @Injectable()
 export class TradesService {
+  private httpDepthUrl = (symbol: string, limit = 1000) =>
+    `https://fapi.binance.com/fapi/v1/depth?symbol=${symbol}&limit=${limit}`;
+
   constructor(
+    private httpService: HttpService,
     private databaseService: DatabaseService,
     private webSocketService: WebSocketService,
   ) {}
 
   async subscribe(symbol: string) {
-    const buffer: Array<any> = [];
+    const aggTradesBuffer: Array<any> = [];
+    const depthBuffer: Array<any> = [];
     this.webSocketService.subscribe(
       symbol,
       async (aggTrade: IAggTrade) => {
-        buffer.push(new AggTrade(aggTrade));
-        if (buffer.length > BUFFER_LENGTH) {
-          await this.flush(buffer.splice(0));
+        aggTradesBuffer.push(new AggTrade(aggTrade));
+        if (aggTradesBuffer.length > AGG_TRADES_BUFFER_LENGTH) {
+          await this.flushAggTrades(aggTradesBuffer.splice(0));
         }
       },
-      (depth) => {
-        Logger.log('received depth');
+      async (depth) => {
+        if (
+          depthBuffer.length &&
+          depthBuffer[depthBuffer.length - 1].u !== depth.pu
+        ) {
+          Logger.warn('sequence broken');
+          await this.flushDepth(depthBuffer.splice(0));
+          await this.setOrderBook(symbol);
+        }
+
+        depthBuffer.push(depth);
+        if (depthBuffer.length > DEPTH_BUFFER_LENGTH) {
+          await this.flushDepth(depthBuffer.splice(0));
+        }
       },
     );
+
+    setInterval(() => {
+      this.setOrderBook(symbol).catch((e) => {
+        Logger.error(e?.message);
+      });
+    }, 1000 * 60 * 10);
   }
 
   unsubscribe(symbol: string) {
@@ -39,9 +64,22 @@ export class TradesService {
    *
    * @param buffer splice of buffer (don't need to splice it again)
    */
-  private async flush(buffer: any[]) {
+  private async flushAggTrades(buffer: any[]) {
     await this.databaseService.aggTrades.createMany({ data: buffer });
+  }
 
-    // save buffer, clear buffer, make snapshot, save snapshot
+  /**
+   *
+   * @param buffer splice of buffer (don't need to splice it again)
+   */
+  private async flushDepth(buffer: any[]) {
+    await this.databaseService.depthUpdates.createMany({ data: buffer });
+  }
+
+  private async setOrderBook(symbol: string) {
+    const snapshot = await firstValueFrom(
+      this.httpService.get(this.httpDepthUrl(symbol)),
+    ).then(({ data }) => ({ ...data, symbol }));
+    await this.databaseService.orderBookSnapshot.create({ data: snapshot });
   }
 }
