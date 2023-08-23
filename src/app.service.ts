dab572c46f6376d62ac668005d01eef1adaff7d1
Rafail1 <ya.rafon-92@ya.ru>
2023-08-23 17:49:03 +0300

    getDepthHistory

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { StarterService } from './modules/starter/starter.service';
+import { DatabaseService } from './modules/database/database.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly starterService: StarterService) {}
+  constructor(
+    private readonly starterService: StarterService,
+    private database: DatabaseService,
+  ) {}
 
   subscribe(symbol: string): void {
     this.starterService.subscribe(symbol);
@@ -17,23 +21,65 @@ export class AppService {
     this.starterService.subscribeAll();
   }
 
-  async test() {
-    for (const symbol of [
-      'ethusdt',
-      'btcusdt',
-      'bnbusdt',
-      'xrpusdt',
-      'dogeusdt',
-      'adausdt',
-      'solusdt',
-      'trxusdt',
-      'maticusdt',
-      '1000shibusdt',
-    ]) {
-      await this.starterService.subscribe(symbol);
-      await new Promise<void>((resolve) => {
-        setTimeout(() => resolve(), 200);
-      });
+  getAggTradesHistory(symbol: string, from: Date, to: Date) {
+    return this.database.aggTrades.findMany({
+      where: {
+        AND: [
+          {
+            E: { gte: from },
+          },
+          {
+            E: { lt: to },
+          },
+        ],
+        s: symbol,
+      },
+    });
+  }
+
+  async getDepthHistory(symbol: string, from: Date, to: Date) {
+    const depth = await this.database.depthUpdates.findMany({
+      where: {
+        AND: [
+          {
+            E: { gte: from },
+          },
+          {
+            E: { lt: to },
+          },
+        ],
+        s: symbol,
+      },
+      orderBy: { U: 'asc' },
+    });
+
+    if (!depth.length) {
+      Logger.warn('depthUpdates not found');
+      return [];
     }
+
+    const snapshot = this.database.orderBookSnapshot.findFirst({
+      where: {
+        lastUpdateId: { lte: depth[0].U },
+        AND: [
+          {
+            E: { gte: from },
+          },
+          {
+            E: { lt: to },
+          },
+        ],
+        symbol,
+      },
+      take: 1,
+      orderBy: { lastUpdateId: 'desc' },
+    });
+
+    if (!snapshot) {
+      Logger.warn('snapshot not found');
+      return [];
+    }
+
+    // logic for data merge
   }
 }
