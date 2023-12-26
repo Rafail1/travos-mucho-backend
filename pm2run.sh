73d4e11be3d81733ab37e31b2342ae00a9010f0e
Rafail1 <ya.rafon-92@ya.ru>
2023-12-26 12:46:10 +0300

    add shell

---
+++ pm2run.sh
@@ -0,0 +1,4 @@
+pm2 --name start-rest start npm -- run start-rest
+pm2 --name start-ob start npm -- run start-ob
+pm2 --name start-sub start npm -- run start-sub
+pm2 --name start-remove-history start npm -- run start-remove-history
