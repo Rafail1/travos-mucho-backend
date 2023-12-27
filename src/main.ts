ae9c2dd527e16764df4ecf0c84e66623b965cc4d
Rafail1 <ya.rafon-92@ya.ru>
2023-12-27 10:08:53 +0300

    pgpass

--- src/main.ts
+++ src/main.ts
@@ -14,28 +14,30 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log', 'debug'],
     cors: true,
   });
-  if (process.argv.includes('start-sub')) {
-    console.log('start-sub');
-    await app
-      .get(AppService)
-      .subscribeAll()
-      .catch((e) => {
-        Logger.error(e);
-        process.exit(1);
-      });
-  } else if (process.argv.includes('start-sub-ob')) {
-    console.log('start-sub-ob');
-    await app.get(OrderBookService).init();
-    await app.get(OrderBookService).setObToAll();
-    await app.get(SnapshotWorkerService).initSnapshotFlow();
-  } else if (process.argv.includes('remove-history')) {
-    console.log('remove-history');
-    await app.get(AppService).removeHistoryInterval();
-    console.log('remove-history done');
-  } else {
-    console.log('8080');
-    await app.listen(8080);
-  }
+  setTimeout(async () => {
+    if (process.argv.includes('start-sub')) {
+      console.log('start-sub');
+      await app
+        .get(AppService)
+        .subscribeAll()
+        .catch((e) => {
+          Logger.error(e);
+          process.exit(1);
+        });
+    } else if (process.argv.includes('start-sub-ob')) {
+      console.log('start-sub-ob');
+      await app.get(OrderBookService).init();
+      await app.get(OrderBookService).setObToAll();
+      await app.get(SnapshotWorkerService).initSnapshotFlow();
+    } else if (process.argv.includes('remove-history')) {
+      console.log('remove-history');
+      await app.get(AppService).removeHistoryInterval();
+      console.log('remove-history done');
+    } else {
+      console.log('8080');
+      await app.listen(8080);
+    }
+  }, 5000);
   console.log(process.argv);
 }
 bootstrap();
