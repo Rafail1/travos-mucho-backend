8b911f207764ca8e9761267071caa4d4d42b7225
Rafail1 <ya.rafon-92@ya.ru>
2024-01-04 18:32:48 +0300

    refacintoring

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,6 @@ async function bootstrap() {
 
   setTimeout(async () => {
     if (process.argv.includes('migrate')) {
-      process.env.PART = process.argv[process.argv.length - 1];
       await app.get(DatabaseService).syncTables();
     } else if (process.argv.includes('start-sub-first')) {
       process.env.PART = 'first';
