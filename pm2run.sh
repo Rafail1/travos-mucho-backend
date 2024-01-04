61a70846d28eef64356cf2c24ea860d4c2e37c40
Rafail1 <ya.rafon-92@ya.ru>
2024-01-04 17:46:00 +0300

    pm2

--- pm2run.sh
+++ pm2run.sh
@@ -2,4 +2,4 @@ npm run build
 npm run migrate
 pm2 --name start-sub-first start npm -- run start-sub-first
 pm2 --name start-remove-history start npm -- run start-remove-history
-pm2 --name run migrate start npm -- run migrate --cron "0 */2 * * *"
+pm2 --name migrate --cron "0 */2 * * *" start npm -- run migrate
