7bc47ff1c2c9ff05815d7e36180911c9b180f2a3
Rafail1 <ya.rafon-92@ya.ru>
2024-01-04 18:09:15 +0300

    refacintoring

--- pm2run.sh
+++ pm2run.sh
@@ -1,5 +1,12 @@
 npm run build
 npm run migrate
-pm2 --name start-sub-first start npm -- run start-sub-first
-pm2 --name start-remove-history start npm -- run start-remove-history
+
 pm2 --name migrate --cron "0 */2 * * *" start npm -- run migrate
+sleep 10
+pm2 --name start-sub-all start npm -- run start-sub-all
+sleep 30
+pm2 --name start-rest start npm -- run start-rest
+sleep 30
+pm2 --name start-ob start npm -- run start-ob
+sleep 20
+pm2 --name start-remove-history start npm -- run start-remove-history
