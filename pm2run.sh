341a65b4442f0c44f62a213cd8768d836a436cb4
Rafail1 <ya.rafon-92@ya.ru>
2024-01-05 16:57:02 +0300

    logging

--- pm2run.sh
+++ pm2run.sh
@@ -1,11 +1,9 @@
 npm run build
 
 pm2 --name migrate start --cron-restart "*/30 * * * *" --no-autorestart npm -- run migrate
-sleep 10
 pm2 --name start-sub-all start npm -- run start-sub-all
-sleep 30
+sleep 10
 pm2 --name start-rest start npm -- run start-rest
-sleep 30
 pm2 --name start-ob start npm -- run start-ob
 sleep 20
 pm2 --name start-remove-history start npm -- run start-remove-history
