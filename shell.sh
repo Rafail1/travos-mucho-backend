d60d583df7af3a60e1663bb2a4906a8df586684c
Rafail1 <ya.rafon-92@ya.ru>
2024-01-10 15:50:13 +0300

    sh

--- shell.sh
+++ shell.sh
@@ -16,17 +16,17 @@ echo "" >> /etc/hosts
 echo "185.251.38.11 scalp24.store" >> /etc/hosts
 echo "185.251.38.12 scalp24.store" >> /etc/hosts
 echo "185.251.38.40 scalp24.store" >> /etc/hosts
-# source /etc/environment
-# source ~/.bashrc
-# nvm install --lts
-# nvm use --lts
-# npm i
+source /etc/environment
+source ~/.bashrc
+nvm install --lts
+nvm use --lts
+npm i
 
-# sudo -u postgres psql
-# CREATE DATABASE "travos-muchos";
-# CREATE USER tramuches WITH ENCRYPTED PASSWORD 'IPFHfr6&63!-';
-# GRANT ALL PRIVILEGES ON DATABASE "travos-muchos" TO tramuches;
-# quit
+sudo -u postgres psql
+CREATE DATABASE "travos-muchos";
+CREATE USER tramuches WITH ENCRYPTED PASSWORD 'IPFHfr6&63!-';
+GRANT ALL PRIVILEGES ON DATABASE "travos-muchos" TO tramuches;
+quit
 
-# npm install pm2@latest -g
-# sh pm2run.sh
+npm install pm2@latest -g
+sh pm2run.sh
