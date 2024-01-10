17cc83c67d35365d53a2b62932caff72d8cfc171
Rafail1 <ya.rafon-92@ya.ru>
2024-01-10 15:29:16 +0300

    upd

--- shell.sh
+++ shell.sh
@@ -5,24 +5,26 @@ sudo apt update
 sudo apt upgrade
 sudo apt install curl 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/.bashrc
-nvm install --lts 
-npm i
+
 sudo apt install postgresql
-sudo -u postgres psql
-CREATE DATABASE "travos-muchos";
-CREATE USER tramuches WITH ENCRYPTED PASSWORD 'IPFHfr6&63!-';
-GRANT ALL PRIVILEGES ON DATABASE "travos-muchos" TO tramuches;
-quit
 
-npm install pm2@latest -g
 
-echo 'PART="' + $part + ' "' >> /etc/environment
+echo 'PART="'$part'"' >> /etc/environment
 echo 'DATABASE_URL="postgresql://tramuches:IPFHfr6&63!-@localhost:5432/travos-muchos"' >> /etc/environment
-source /etc/environment
 
 echo "185.251.38.11 scalp24.store" >> /etc/hosts
 echo "185.251.38.12 scalp24.store" >> /etc/hosts
 echo "185.251.38.40 scalp24.store" >> /etc/hosts
+# source /etc/environment
+# source ~/.bashrc
+# nvm install --lts
+# npm i
 
-sh pm2run.sh
+# sudo -u postgres psql
+# CREATE DATABASE "travos-muchos";
+# CREATE USER tramuches WITH ENCRYPTED PASSWORD 'IPFHfr6&63!-';
+# GRANT ALL PRIVILEGES ON DATABASE "travos-muchos" TO tramuches;
+# quit
+
+# npm install pm2@latest -g
+# sh pm2run.sh
